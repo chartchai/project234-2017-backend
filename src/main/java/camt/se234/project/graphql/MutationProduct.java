c353a726274bd9fffeab5845407ef0332321dbaa
Chartchai Doungsa-ard <chartchai.d@cmu.ac.th>
2018-04-05 12:19:50 +0700

    add graphql setting

---
+++ src/main/java/camt/se234/project/graphql/MutationProduct.java
@@ -0,0 +1,6 @@
+package camt.se234.project.graphql;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+
+public class MutationProduct  implements GraphQLMutationResolver {
+}
---
+++ src/main/java/camt/se234/project/graphql/QueryProduct.java
@@ -0,0 +1,6 @@
+package camt.se234.project.graphql;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+
+public class QueryProduct  implements GraphQLQueryResolver {
+}
