1630ebac07b42e1d610e8ddc74822aa3ed20c5c9
DESKTOP-OBSRVEA\dto-X1 <chartchai.d@cmu.ac.th>
2018-04-10 14:51:40 +0700

    add the image controller

--- src/main/java/camt/se234/project/graphql/QueryProduct.java
+++ src/main/java/camt/se234/project/graphql/QueryProduct.java
@@ -2,15 +2,19 @@ package camt.se234.project.graphql;
 
 import camt.se234.project.entity.Product;
 import camt.se234.project.entity.SaleTransaction;
+import camt.se234.project.service.ProductService;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class QueryProduct  implements GraphQLQueryResolver {
+    @Autowired
+    ProductService productService;
     public List<Product> getProducts(){
-        return null;
+        return productService.getAllProducts();
     }
 
     public List<SaleTransaction> getSaleTransactions(){
